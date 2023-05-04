7a2ebdb69690f732604e2c21561946c673831638
Andrew Webster <webstean@gmail.com>
2023-05-04 20:26:41 +1000

    egersfdfse

--- wslsetup1.sh
+++ wslsetup1.sh
@@ -37,7 +37,8 @@ if [ -f /usr/bin/apt ] ; then
     # Install Microsoft tools
     ${CMD_INSTALL} ttf-mscorefonts-installer
     ${CMD_INSTALL} azure-functions-core-tools
-    ${CMD_INSTALL} mssql-tools sqlcmd
+    # ${CMD_INSTALL} mssql-tools 
+    ${CMD_INSTALL} sqlcmd
     ${CMD_INSTALL} powershell
     ${CMD_INSTALL} msopenjdk-17
     
