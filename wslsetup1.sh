3180c6a5bbfb1b96c9788c26cf394e3990d1466a
Andrew Webster <webstean@gmail.com>
2023-05-04 20:23:22 +1000

    sadssa

--- wslsetup1.sh
+++ wslsetup1.sh
@@ -33,6 +33,7 @@ if [ -f /usr/bin/apt ] ; then
     # Skip ELA prompt - I hope
     echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 
+    export ACCEPT_EULA=Y
     # Install Microsoft tools
     ${CMD_INSTALL} ttf-mscorefonts-installer
     ${CMD_INSTALL} azure-functions-core-tools
