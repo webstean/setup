7ca8bc9dfe32a15d2933d455386191d388e643c2
Andrew Webster <webstean@gmail.com>
2023-06-12 19:16:48 +1000

    Update wslsetup2.sh

--- wsl/wslsetup2.sh
+++ wsl/wslsetup2.sh
@@ -191,12 +191,11 @@ oracleinstantclientinstall() {
     ## use Oraclw SQL statement to create CSV files you can export and import into some else (like sqllite)
     ## https://www.dba-oracle.com/t_export%20table_to_csv.htm
     # set heading off
-    spool myfile.csv
-    select
-   col1|','||col2 from my_tables;
-   set colsep ','
-   select * from my_table;
-    spool off;
+    # spool myfile.csv
+    # select col1|','||col2 from my_tables;
+    # set colsep ','
+    # select * from my_table;
+    # spool off;
     
     return 0
 }
