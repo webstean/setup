d063d6cab201f8001d5e9ebf82b4de2a0bab56fd
andrewwebster-hso <112995392+andrewwebster-hso@users.noreply.github.com>
2023-06-20 15:36:43 +1000

    Update wslsetup1.sh

--- wsl/wslsetup1.sh
+++ wsl/wslsetup1.sh
@@ -4,73 +4,10 @@
 [ "$DEBUG" == 'true' ] && set -x
 # set +x to disable
 
-## Check if WSL2, enable systemd etc via wsl.conf
-if [[ $(grep -i WSL2 /proc/sys/kernel/osrelease) ]] ; then
-    if [ -f /etc/wsl.conf ] ; then sudo rm -f /etc/wsl.conf ; fi
-    sudo sh -c 'echo [boot]                     >>  /etc/wsl.conf'
-    sudo sh -c 'echo systemd=true               >>  /etc/wsl.conf'
-
-    sudo sh -c 'echo [automount]                >>  /etc/wsl.conf'
-    sudo sh -c 'echo enabled = true             >>  /etc/wsl.conf'
-    sudo sh -c 'echo root = \/mnt               >>  /etc/wsl.conf'
-    ## copy from: https://github.com/WhitewaterFoundry/Fedora-Remix-for-WSL/blob/master/linux_files/wsl.conf
-    sudo sh -c 'echo options = "metadata,uid=1000,gid=1000,umask=22,fmask=11,case=off" >>  /etc/wsl.conf'
-    sudo sh -c 'echo mountFsTab = true          >>  /etc/wsl.conf'
-
-    sudo sh -c 'echo [interop]                  >>  /etc/wsl.conf'
-    sudo sh -c 'echo enabled = true             >>  /etc/wsl.conf'
-    sudo sh -c 'echo appendWindowsPath = true   >>  /etc/wsl.conf'
-
-    sudo sh -c 'echo [network]                  >>  /etc/wsl.conf'
-    sudo sh -c 'echo generateResolvConf = true  >>  /etc/wsl.conf'
-    sudo sh -c 'echo generateHosts = true       >>  /etc/wsl.conf'
-    
-    echo "USERNAME = [${USERNAME}]"
-    if [ ! -z "${USERNAME+x}" ] ; then
-        echo "Setting default WSL user as: $USERNAME"
-        sudo sh -c 'echo [user]                     >>  /etc/wsl.conf'
-        sudo sh -c 'echo default = ${USERNAME}      >>  /etc/wsl.conf'
-    fi
-else
-    echo "Sorry, only supports WSL2 (not WSL1)"
-    exit 1
-fi
-
 ## Global environmment variables (editable)
 sudo sh -c "echo export FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT=1  >  /etc/profile.d/global-variables.sh"
 sudo sh -c "echo # export AW1=AW1       >>  /etc/profile.d/global-variables.sh"
 # Turn off Microsoft telemetry for Azure Function Tools
-
-# Environent Variables for proxy support
-sudo sh -c 'echo "## Web Proxy Setup - edit as required"                               >  /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "## Squid default port is 3128, but many setup the proxy on port 80,8000,8080" >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "anon_web-proxy() {"                                                       >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  ## Set variable for proxy and port"                                >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  port=3128"                                                         >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  webproxy=webproxy.local"                                           >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  ## Proxy Exceptions"                                               >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  export NO_PROXY=localhost,127.0.0.1,::1,192.168.0.0/16,10.0.0.0/8" >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  ## Anonymous Proxy"                                                >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  export {http,https,ftp}_proxy=http://\${webproxy}:\${port}"        >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  export HTTPS_PROXY=http://\${webproxy}:\${port}"                   >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  export FTP_PROXY=http://\${webproxy}:\${port}"                     >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  return;"                                                           >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "}"                                                                   >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "auth_web-proxy() {"                                                  >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  ## Set variable for proxy and port"                                >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  port=3128"                                                         >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  webproxy=webproxy.local"                                           >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  ## Set variables for authenticated proxy"                          >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  USERN=UserName"                                                    >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  @ME=Password"                                                      >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  ## Proxy Exceptions"                                               >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  export NO_PROXY=localhost,127.0.0.1,::1,192.168.0.0/16,10.0.0.0/8" >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  export {http,https,ftp}_proxy=http://\${USERN}:\${@ME}\${webproxy}:\${port}/"  >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "  return;"                                                           >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "}"                                                                   >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "# anon_web-proxy()"                                                  >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "# auth_web-proxy()"                                                  >> /etc/profile.d/web-proxy.sh'
-sudo sh -c 'echo "export extaddr=$(curl -s ifconfig.me)"                               >> /etc/profile.d/web-proxy.sh'
 
 # leave along at C.UTF-8 for maximum compatiblity
 ##sudo locale-gen "C.UTF-8"
