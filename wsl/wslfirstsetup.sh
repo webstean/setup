15db6b14c6137b28c5ea5872058520602f56ff92
andrewwebster-hso <112995392+andrewwebster-hso@users.noreply.github.com>
2023-06-20 15:39:49 +1000

    Update wslfirstsetup.sh

--- wsl/wslfirstsetup.sh
+++ wsl/wslfirstsetup.sh
@@ -37,6 +37,17 @@ if [[ $(grep -i WSL2 /proc/sys/kernel/osrelease) ]] ; then
         sh -c 'echo [user]                     >>  /etc/wsl.conf'
         sh -c 'echo default = ${USERNAME}      >>  /etc/wsl.conf'
     fi
+    # Enable sudo for all users - by modifying /etc/sudoers
+    if ! (sudo grep NOPASSWD:ALL /etc/sudoers  > /dev/null 2>&1 ) ; then 
+        # Everyone
+        bash -c "echo '#Everyone - WSL' | sudo EDITOR='tee -a' visudo"
+        bash -c "echo '%sudo ALL=(ALL:ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo"
+        # AAD (experimental)
+        bash -c "echo '#Azure AD - WSL' | sudo EDITOR='tee -a' visudo"
+        bash -c "echo '%sudo aad_admins=(ALL:ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo"
+    else
+        echo "/etc/sudoers edit not required!"
+    fi
 else
     echo "Sorry, only supports WSL2 (not WSL1)"
     exit 1
@@ -53,7 +64,7 @@ else
     exit 1
 fi
 
-# Environent Variables for proxy support
+## Environent Variables for proxy support
 sh -c 'echo "## Web Proxy Setup - edit as required"                               >  /etc/profile.d/web-proxy.sh'
 sh -c 'echo "## Squid default port is 3128, but many setup the proxy on port 80,8000,8080" >> /etc/profile.d/web-proxy.sh'
 sh -c 'echo "anon_web-proxy() {"                                                  >> /etc/profile.d/web-proxy.sh'
@@ -80,6 +91,8 @@ sh -c 'echo "  export NO_PROXY=localhost,127.0.0.1,::1,192.168.0.0/16,10.0.0.0/8
 sh -c 'echo "  export {http,https,ftp}_proxy=http://\${USERN}:\${@ME}\${webproxy}:\${port}/"  >> /etc/profile.d/web-proxy.sh'
 sh -c 'echo "  return;"                                                           >> /etc/profile.d/web-proxy.sh'
 sh -c 'echo "}"                                                                   >> /etc/profile.d/web-proxy.sh'
+sh -c 'echo "## uncomment for unauthenticated proxy ##"                           >> /etc/profile.d/web-proxy.sh'
 sh -c 'echo "# anon_web-proxy()"                                                  >> /etc/profile.d/web-proxy.sh'
+sh -c 'echo "## uncomment for authenticated proxy ##"                             >> /etc/profile.d/web-proxy.sh'
 sh -c 'echo "# auth_web-proxy()"                                                  >> /etc/profile.d/web-proxy.sh'
 sh -c 'echo "export extaddr=$(curl -s ifconfig.me)"                               >> /etc/profile.d/web-proxy.sh'
