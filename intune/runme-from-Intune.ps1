9055c270fa3cfda447703678532c3ec0f2cb9442
Andrew Webster <webstean@gmail.com>
2025-10-06 12:20:21 +1100

    Update runme-from-Intune.ps1

--- intune/runme-from-Intune.ps1
+++ intune/runme-from-Intune.ps1
@@ -133,15 +133,15 @@ if ($winget) {
     }
 }
 try {
-    & "Config-Normal-Machine.ps1",
-    & "Install-Global-Secure-Access-Client.ps1",
-    & "Install-Windows-Admin-Centre.ps1",
-    & "Install-Developer-Fonts.ps1",
-    & "Install-Developer-System.ps1", ## installs dotnet, that we need later
+    & ".\Config-Normal-Machine.ps1",
+    & ".\Install-Global-Secure-Access-Client.ps1",
+    & ".\Install-Windows-Admin-Centre.ps1",
+    & ".\Install-Developer-Fonts.ps1",
+    & ".\Install-Developer-System.ps1", ## installs dotnet, that we need later
 
-    & "Install-Developer-PowerShellModules.ps1",
-    & "Install-Developer-User.ps1",
-    & "Winget-Config-Developer.ps1"
+#    & ".\Install-Developer-PowerShellModules.ps1",
+#    & ".\Install-Developer-User.ps1",
+#    & ".\Winget-Config-Developer.ps1"
 
 catch {
     Write-Error "Error executing ${script}: $_"
