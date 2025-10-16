2c4e998c1a3b525586ce15df61ccdd34476a2096
Andrew Webster <webstean@gmail.com>
2025-10-16 18:50:30 +1100

    Refactor winget configuration commands in script

--- intune/Winget-Config-Developer.ps1
+++ intune/Winget-Config-Developer.ps1
@@ -1,12 +1,13 @@
 #Requires -RunAsAdministrator
 
-if (-not (Get-Module -ListAvailable -Name Microsoft.WinGet.Client)) {
-    Install-Module Microsoft.WinGet.Client -Force -Scope CurrentUser
-}
-winget configure developer.winget --accept-configuration-agreements --verbose-logs
+winget configure validate --file developer.winget --ignore-warnings --disable-interactivity --verbose-logs
+winget configure --file developer.winget --accept-configuration-agreements --suppress-initial-details --ignore-warnings --disable-interactivity --verbose-logs
 
 exit 0
 
+if (-not (Get-Module -ListAvailable -Name Microsoft.WinGet.Client)) {
+    Install-Module Microsoft.WinGet.Client -Force -Scope CurrentUser
+}
 if (-not (Get-Module -Name Microsoft.WinGet.Client)) {
     Import-Module Microsoft.WinGet.Client
 }
