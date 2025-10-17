d9326e6e78831d5750652391a36c6584b72d255f
Andrew Webster <webstean@gmail.com>
2025-10-17 14:50:20 +1100

    Update Winget-Config-Developer.ps1

--- intune/Winget-Config-Developer.ps1
+++ intune/Winget-Config-Developer.ps1
@@ -2,7 +2,9 @@
 
 #winget configure validate --file developer.winget --ignore-warnings --disable-interactivity --verbose-logs
 #winget configure show     --file developer.winget --ignore-warnings --disable-interactivity --verbose-logs
-winget configure          --file developer.winget --accept-configuration-agreements --suppress-initial-details --ignore-warnings --disable-interactivity --verbose-logs
+winget configure          --file developer.winget --accept-configuration-agreements --suppress-initial-details --disable-interactivity --verbose-logs
+
+## get-childitem     $env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\DiagOutputDir\
 
 exit 0
 
