e100e9dcb5b813d6d43e8425e04648ffdb89c590
Andrew Webster <webstean@gmail.com>
2025-10-24 19:53:15 +1100

    Update Windows-Update-And-Reboot.ps1

--- intune/run/Windows-Update-And-Reboot.ps1
+++ intune/run/Windows-Update-And-Reboot.ps1
@@ -1,14 +1,21 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
-    Install-Module -Name PSWindowsUpdate -Force -Scope AllUsers
+# Check if module is already installed
+$installed = Get-PSResource -Name PSWindowsUpdate -ErrorAction SilentlyContinue
+if ($null -eq $installed) {
+    Write-Host "Module '$ModuleName' not found. Installing..." -ForegroundColor Green
+    Install-PSResource PSWindowsUpdate
+} else {
+    Write-Host "Module '$ModuleName' found. Updating..." -ForegroundColor Cyan
+    Update-PSResource PSWindowsUpdate
 }
-Import-Module PSWindowsUpdate
-
+Import-Module PSWindowsUpdate -Force
 # Install all available updates silently
 Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot
 
 # Reboot automatically if required
 if (Get-WURebootStatus) {
+    Write-Host "**REboot-Required**"
+    Sleep 30
     Restart-Computer -Force
 }
