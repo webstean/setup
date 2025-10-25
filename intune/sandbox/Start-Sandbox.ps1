eda7f59c94e5f7d38e211e7c0f72c938530db344
Andrew Webster <webstean@gmail.com>
2025-10-25 15:23:30 +1100

    Add Start-WindowsSandbox function to launch sandbox

---
+++ intune/sandbox/Start-Sandbox.ps1
@@ -0,0 +1,25 @@
+Function Start-WindowsSandbox {
+    [cmdletbinding(DefaultParameterSetName = "config")]
+    [alias("wsb")]
+    Param(
+        [Parameter(ParameterSetName = "config")]
+        [ValidateScript({Test-Path $_})]
+        [string]$Configuration = "C:\scripts\WinSandBx.wsb",
+        [Parameter(ParameterSetName = "normal")]
+        [switch]$NoSetup
+    )
+
+    Write-Verbose "Starting $($myinvocation.mycommand)"
+
+    if ($NoSetup) {
+        Write-Verbose "Launching default WindowsSandbox.exe"
+        c:\windows\system32\WindowsSandbox.exe
+    }
+    else {
+        Write-Verbose "Launching WindowsSandbox using configuration file $Configuration"
+        Invoke-Item $Configuration
+    }
+
+    Write-Verbose "Ending $($myinvocation.mycommand)"
+}    
+Start-WindowsSandbox
