705f38bfb0ccc6023a7f6684efcf3fdb32c5a2ba
Andrew Webster <webstean@gmail.com>
2025-10-29 11:42:26 +1100

    Update good_profile.ps1

--- intune/good_profile.ps1
+++ intune/good_profile.ps1
@@ -707,5 +707,10 @@ if ($IsLanguagePermissive) {
     if (Get-Module -ListAvailable -Name Terminal-Icons | Out-Null ) {
         Import-Module Terminal-Icons -ErrorAction SilentlyContinue
     }
+    $raw = $Host.UI.RawUI
+    $raw.BufferSize = New-Object System.Management.Automation.Host.Size(
+    [Math]::Max($raw.BufferSize.Width, 160),  # width
+    10000                                     # height
+    )
 }
 
