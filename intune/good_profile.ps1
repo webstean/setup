68f5f07fffb5b4e68514b5b8b8d51affdd6c4951
Andrew Webster <webstean@gmail.com>
2025-11-05 15:19:12 +1100

    Update Enable-PIMRole comment to use -Wait

--- intune/good_profile.ps1
+++ intune/good_profile.ps1
@@ -1047,7 +1047,7 @@ function Enable-PIMRole {
         throw "Failed to activate PIM role ($RoleName): $($_.Exception.Message)"
     }
 }
-## Enable-PIMRole -ByName
+## Enable-PIMRole -Wait
 ## Connect-MgGraph -NoWelcome"
 ## $user = Get-MgUserMe
 
