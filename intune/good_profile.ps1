46a54aeb1ee7814eaea27e60f6e8762384aa252a
Andrew Webster <webstean@gmail.com>
2025-11-06 19:15:26 +1100

    Update good_profile.ps1

--- intune/good_profile.ps1
+++ intune/good_profile.ps1
@@ -25,7 +25,7 @@ function Update-Profile-Force {
     if (Test-Path $PROFILE -ErrorAction SilentlyContinue) {
         $oldContent = Get-Content -Path $PROFILE -Raw -Encoding ASCII
 
-        if ($oldContent -eq $newContent) {
+        if ($oldContent -eq $newContent.content) {
             Write-Host "The downloaded file is identical to the existing one - no update needed." -ForegroundColor Yellow
         } else {
             $newContent | Out-File -FilePath $PROFILE -Encoding ASCII
