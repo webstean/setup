0c04d03c01af596a1beb21547dfed36ce86ce164
Andrew Webster <webstean@gmail.com>
2025-11-12 20:08:07 +1100

    Create auth.cs

---
+++ intune/auth.cs
@@ -0,0 +1,22 @@
+using Microsoft.Identity.Client;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        var app = PublicClientApplicationBuilder
+            .Create("14d82eec-204b-4c2f-b7e8-296a70dab67e")
+            .WithDefaultRedirectUri()
+            .WithBroker()     // <-- This tells MSAL to use WAM
+            .Build();
+
+        var result = await app.AcquireTokenInteractive(
+                new[] { "https://graph.microsoft.com/User.Read" })
+            .ExecuteAsync();
+
+        Console.WriteLine($"Access token: {result.AccessToken.Substring(0,40)}...");
+    }
+}
+
