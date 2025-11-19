bb2c01009e2d170ea4f60f37f0a8800809b4ef45
Andrew Webster <webstean@gmail.com>
2025-11-19 12:01:15 +1100

    Update Install-Developer-PowershellModules.ps1

--- intune/Install-Developer-PowershellModules.ps1
+++ intune/Install-Developer-PowershellModules.ps1
@@ -186,7 +186,8 @@ Get-Command -Module PnP.PowerShell
 ## Sites.ReadWrite.All     – read/write to all site collections the user can access.
 ## Sites.Manage.All        - lets you manage site permissions via Graph.         –
 #if ( $env:SHAREPOINT_ADMIN ) {
-#    Connect-PnPOnline -Url $env:AZURE_SHAREPOINT_ADMIN -AccessToken $env:ACCESS_TOKEN
+#    Connect-PnPOnline -Url "https://${env:AZURE_SHAREPOINT_ADMIN}.sharepoint.com" -Interactive
+#    Connect-PnPOnline -Url "https://${env:AZURE_SHAREPOINT_ADMIN}.sharepoint.com" -AccessToken $env:ACCESS_TOKEN
 #    Get-PnpConnection
 #    $ACCESS_TOKEN = Get-PnPAccessToken
 #    Get-PnPTenant
