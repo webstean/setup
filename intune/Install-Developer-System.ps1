efdf4d84b5954b58d96d881efa997bf3eaeebfae
Andrew Webster <webstean@gmail.com>
2026-07-14 19:53:06 +1000

    Update Install-Developer-System.ps1

--- intune/Install-Developer-System.ps1
+++ intune/Install-Developer-System.ps1
@@ -1222,6 +1222,8 @@ if (Test-Path $wauConfig) {
 
 Add-Type -AssemblyName System.Windows.Forms
 
+
+
 openfiles /local on
 ## SUCCESS: The system global flag 'maintain objects list' is enabled.
 ##         This will take effect after the system is restarted.
