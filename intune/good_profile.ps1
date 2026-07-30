6c48896f7e5e0e97af61503171e9acd021d481b1
Andrew Webster <webstean@gmail.com>
2026-07-30 16:00:37 +1000

    Update good_profile.ps1

--- intune/good_profile.ps1
+++ intune/good_profile.ps1
@@ -4049,3 +4049,78 @@ function Get-AzVmSku {
     $results | Format-Table -AutoSize
     "Count: $($results.Count) in $($Location)"
 }
+
+function Get-QuickXorHashFromFile {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$FilePath
+    )
+
+    $BitsInLastCell = 32
+    $Shift = 11
+    $Threshold = 600
+    $WidthInBits = 160
+
+    $data = @(0..2 | ForEach-Object { [UInt64]0 })
+    $lengthSoFar = 0
+    $shiftSoFar = 0
+
+    $buffer = [System.IO.File]::ReadAllBytes($FilePath)
+    $lengthSoFar = $buffer.Length
+
+    $ibStart = 0
+    $cbSize = $buffer.Length
+
+    $currentShift = $shiftSoFar
+    $vectorArrayIndex = [math]::Floor($currentShift / 64)
+    $vectorOffset = $currentShift % 64
+    $iterations = [math]::Min($cbSize, $WidthInBits)
+
+    for ($i = 0; $i -lt $iterations; $i++) {
+        $isLastCell = $vectorArrayIndex -eq ($data.Length - 1)
+        $bitsInVectorCell = $isLastCell ? $BitsInLastCell : 64
+
+        if ($vectorOffset -le ($bitsInVectorCell - 8)) {
+            for ($j = $ibStart + $i; $j -lt ($cbSize + $ibStart); $j += $WidthInBits) {
+                $data[$vectorArrayIndex] = $data[$vectorArrayIndex] -bxor ([UInt64]$buffer[$j] -shl $vectorOffset)
+            }
+        } else {
+            $index1 = $vectorArrayIndex
+            $index2 = $isLastCell ? 0 : ($vectorArrayIndex + 1)
+            $low = $bitsInVectorCell - $vectorOffset
+
+            $xoredByte = 0
+            for ($j = $ibStart + $i; $j -lt ($cbSize + $ibStart); $j += $WidthInBits) {
+                $xoredByte = $xoredByte -bxor $buffer[$j]
+            }
+
+            $data[$index1] = $data[$index1] -bxor ([UInt64]$xoredByte -shl $vectorOffset)
+            $data[$index2] = $data[$index2] -bxor ([UInt64]$xoredByte -shr $low)
+        }
+
+        $vectorOffset += $Shift
+        while ($vectorOffset -ge $bitsInVectorCell) {
+            $vectorArrayIndex = $isLastCell ? 0 : ($vectorArrayIndex + 1)
+            $vectorOffset -= $bitsInVectorCell
+        }
+    }
+
+    $shiftSoFar = ($shiftSoFar + $Shift * ($cbSize % $WidthInBits)) % $WidthInBits
+
+    # Finalize hash
+    $rgb = New-Object byte[] 20
+    for ($i = 0; $i -lt $data.Length - 1; $i++) {
+        [System.Buffer]::BlockCopy([BitConverter]::GetBytes($data[$i]), 0, $rgb, $i * 8, 8)
+    }
+
+    $lastIndex = ($data.Length - 1)
+    [System.Buffer]::BlockCopy([BitConverter]::GetBytes($data[$lastIndex]), 0, $rgb, $lastIndex * 8, $rgb.Length - ($lastIndex * 8))
+
+    $lengthBytes = [BitConverter]::GetBytes([Int64]$lengthSoFar)
+    for ($i = 0; $i -lt $lengthBytes.Length; $i++) {
+        $rgb[($WidthInBits / 8) - $lengthBytes.Length + $i] = $rgb[($WidthInBits / 8) - $lengthBytes.Length + $i] -bxor $lengthBytes[$i]
+    }
+
+    return [Convert]::ToBase64String($rgb)
+}
+
